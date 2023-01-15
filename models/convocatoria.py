562585906d5d93711a83a2dd07bf8ca4dd1fef75
Albert <albertpm95@outlook.com>
2023-01-15 13:39:16 +0100

    Modelo convocatoria

---
+++ models/convocatoria.py
@@ -0,0 +1,42 @@
+from datetime import date, time
+
+
+class ComprensionLectora():
+    peso_parte: int
+    observaciones: str
+    tarea_1: int
+    tarea_2: int
+    tarea_3: int
+
+
+class ComprensionAuditiva():
+    peso_parte: int
+    observaciones: str
+    tarea_1: int
+    tarea_2: int
+    tarea_3: int
+
+
+class ExpresionEscrita():
+    peso_parte: int
+    observaciones: str
+    tarea_1: int  # Eficacia C., Coherencia, Alcance, Correccion
+    tarea_2: int  # Eficacia C., Coherencia, Alcance, Correccion
+
+
+class ExpresionOral():
+    peso_parte: int
+    observaciones: str
+    tarea_1: int  # Eficacia C., Coherencia, Alcance, Correccion
+    tarea_2: int  # Eficacia C., Coherencia, Alcance, Correccion
+
+
+class Convocatoria():
+    id: int
+    tipo: str  # Ordinaria | Extraordinaria
+    fecha: date
+    hora: time
+    resultado_lectura_escritura: int
+    resultado_destrezas_orales: int
+    resultado_global: int
+    calificacion: int
