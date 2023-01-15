2a9554f7578f67bc6f9e430eec96fac42c5d6890
Albert <albertpm95@outlook.com>
2023-01-15 13:39:27 +0100

    Primera iteracion de login con OAuth2

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+from models.user import Admin, Corrector, Gestor, User
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+fake_db_users = [
+    User(id=1, nombre='Bob', apellido='Swanson',
+         usuario='bob_swanson', password=1234, rol='admin'),
+    User(id=2, nombre='Rick', apellido='Sanchez',
+         usuario='rick_sanchez', password=1234, rol='gestor'),
+    User(id=3, nombre='Morty', apellido='Smith',
+         usuario='morty_smith', password=1234, rol='corrector'),
+]
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/users/")
+async def get_users(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, fake_db_users)
+    print(id)
+    try:
+        return (users)[0]
+    except:
+        return {'error': 'No se ha encontrado al usuario.'}
