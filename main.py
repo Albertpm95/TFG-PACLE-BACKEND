02f890c1d69cc2f78ee519e60b089f56a4b1f24c
Albert <albertpm95@outlook.com>
2023-01-20 15:55:43 +0100

    Updated main

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from sqlalchemy.orm import sessionmaker
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 import crud
 import models
 import schemas
-
 from database import get_engine
 
 engine = get_engine()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-models.Base.metadata.create_all(engine)
+models.Base.metadata.create_all(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 app = FastAPI()
 
 
 def get_db():
-    db = Session()
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -28,10 +26,26 @@ def get_db():
 @app.get("/alumnos/", response_model=list[schemas.Alumno])
 def get_alumnos(db: Session = Depends(get_db)):
     alumnos = crud.get_alumnos(db=db)
+    if not alumnos:
+        return JSONResponse(content={"detail": "No se han encontrado alumnos."},
+                            status_code=204)
     return alumnos
 
 
 @app.get("/convocatorias/", response_model=list[schemas.Convocatoria])
 def get_convocatorias(db: Session = Depends(get_db)):
     convocatorias = crud.get_convocatorias(db=db)
+    if not convocatorias:
+        return JSONResponse(content={"detail": "No se han encontrado alumnos."},
+                            status_code=204)
+    print('Convocatorias', convocatorias)
     return convocatorias
+
+
+@app.post("/create/alumno", response_model=schemas.Alumno, status_code=201)
+def new_alumno(alumno: schemas.Alumno, db: Session = Depends(get_db)):
+    db_alumno = models.Alumno(name=alumno.name)
+    db.add(db_alumno)
+    db.commit()
+    db.refresh(db_alumno)
+    return db_alumno
