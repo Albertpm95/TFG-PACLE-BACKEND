1e5f4c97d1b23d5f3474152b3962d65bbf229664
Albert <albertpm95@outlook.com>
2023-01-20 18:42:21 +0100

    Nuevos modelo y schema + config

--- models.py
+++ models.py
@@ -1,9 +1,31 @@
-from sqlalchemy.sql.expression import null
-from database import Base
-from sqlalchemy import String, Boolean, Integer, Column, Text
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Table
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+presentados_acta = Table(
+    "presentados_acta",
+    Column("id_alumno", ForeignKey("alumnos.id"), primary_key=True),
+    Column("id_acta", ForeignKey("actas.id"), primary_key=True)
+)
 
 
-class Convocatoria(Base):
-    __tablename__ = 'items'
-    id_convocatoria = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
+class Acta(Base):
+    __tablename__ = 'actas'
+
+    id = Column(String, primary_key=True, index=True)
+    lenguage = Column(String)
+    tipo = Column(String)
+    fecha = Column(DateTime)
+    presentados = relationship(
+        "Alumno", secondary=presentados_acta, back_populates=True)
+
+
+class Alumno(Base):
+    __tablename__ = 'alumnos'
+
+    id = Column(String, primary_key=True, index=True)
+    nombre = Column(String)
+    apellidos = Column(String)
+    examinado_convocatorias = relationship(
+        "Acta", secondary=presentados_acta, back_populates=True)
