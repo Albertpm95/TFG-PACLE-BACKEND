da39e430cc41a325ae32dcae004c0514e1cfbc3e
Albert <albertpm95@outlook.com>
2023-02-04 13:07:07 +0100

    Quitados imports sin usar

--- models/actas.py
+++ models/actas.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Table
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
 
 from db.base_class import Base
