776a92e94b946e4bd03d05059ee591a159c66afa
Albert <albertpm95@outlook.com>
2023-02-05 16:11:54 +0100

    Creation of constants file for fake db

---
+++ constants.py
@@ -0,0 +1,38 @@
+lista_acciones_por_rol = [
+    {"action_label": "Crear una convocatoria"},  # Form vacio -> Post
+    {"action_label": "Listar convocatorias"},  # Get todas las convocatorias
+    {"action_label": "Cargar CSV"},  # Put fichero CSV para crear algo
+    # Get todos los alumnos # Entiendo que para los correctores
+    {"action_label": "Listar alumnos"},
+    {"action_label": "Listar convocatiroas activas"}
+]
+fake_usuarios_DB = [
+    {
+        "name": "Rick",
+        "lastname": "Sanchez",
+        "password": "1234",
+        "username": "rick_sanchez",
+        "rol": "gestor",
+    },
+    {
+        "name": "Morty",
+        "lastname": "Smith",
+        "password": "1234",
+        "username": "morty_smith",
+        "rol": "corrector",
+    },
+    {
+        "name": "Jerry",
+        "lastname": "Smith",
+        "password": "1234",
+        "username": "jerry_smith",
+        "rol": "administrador",
+    },
+]
+fake_actas_DB = [
+    {}
+]
+
+fake_alumnos_DB = [
+
+]
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_login import LoginManager
-
-import environment
-from schemas.usuario import Usuario, UsuarioLogin
 
 app = FastAPI()
 
-manager = LoginManager(environment.SECRET, "/login")
 origins = ["http://localhost:4200",
            "http://localhost:4200/", "http://localhost:4200/*"]
 
@@ -19,64 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-fake_DB = [
-    {
-        "name": "Rick",
-        "lastname": "Sanchez",
-        "password": "1234",
-        "username": "rick_sanchez",
-        "rol": "gestor",
-    },
-    {
-        "name": "Morty",
-        "lastname": "Smith",
-        "password": "1234",
-        "username": "morty_smith",
-        "rol": "corrector",
-    },
-    {
-        "name": "Jerry",
-        "lastname": "Smith",
-        "password": "1234",
-        "username": "jerry_smith",
-        "rol": "administrador",
-    },
-]
 
-
-@app.get("/")
+@ app.get("/")
 async def root():
-    return {"message": "Root"}
-
-
-# Tests
-
-
-@app.get("/get_plural/")
-async def get_plural():
-    return fake_DB
-
-
-@app.get("/get_1/{acta_id}")
-async def get_1(acta_id):
-    return {"Get ": acta_id}
-
-
-@app.get("/get_2/{acta_id}")
-async def get_2(acta_id: int):
-    return {"Int acta_id": acta_id}
-
-
-@app.post("/post_1")
-async def post_1(username: str):
-    return {'message', 'Hola ' + username}
-
-
-@app.post("/post_2")
-async def post_2(username: str, password: str):
-    return {'message', 'Hola ', username, ' tu contraseña es ', password}
-
-
-@app.post("/post_3")
-async def post_3(usuario: UsuarioLogin):
-    return {'message', 'Hola Usuario: ', ' tu username es: ', usuario.username, ' y tu contraseña es ', usuario.password}
+    return {"message": "Hay conexion con el servidor"}
