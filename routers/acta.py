b390b2176242e8150452efa09d476e0201e75a09
Albert <albertpm95@outlook.com>
2023-02-08 19:10:39 +0100

    Fix acta models

--- routers/acta.py
+++ routers/acta.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from constants import fake_actas_DB
-from schemas.acta import Acta
+from schemas.acta import ActaBase, IDIOMAS_DISPONIBLES, TIPOS_ACTA
+from typing import get_args
+
 router = APIRouter()
 
 
-@router.get("/actas")
+@router.get("/actas/list")
 async def recuperar_actas():
     actas = []
     actas = fake_actas_DB
@@ -12,6 +14,16 @@ async def recuperar_actas():
 
 
 @router.get("/actas")
-async def recuperar_acta(acta_id: str):
-    acta = filter(lambda x: x.acta_id == acta_id, fake_actas_DB)
+async def recuperar_acta(id_acta: str):
+    acta = filter(lambda x: x.id_acta == id_acta, fake_actas_DB)
     return acta
+
+
+@router.get("/actas/idiomas")
+async def recuperar_idiomas_disponibles():
+    return get_args(IDIOMAS_DISPONIBLES)
+
+
+@router.get("/actas/tipos")
+async def recuperar_tipos_acta():
+    return get_args(TIPOS_ACTA)
--- schemas/acta.py
+++ schemas/acta.py
@@ -4,11 +4,15 @@ from pydantic import BaseModel
 
 from schemas.alumno import Alumno
 
+IDIOMAS_DISPONIBLES = Literal['Español', 'English',
+                              'Català', 'Français', 'Chainese', 'Deutsch']
+TIPOS_ACTA = Literal['Ordinaria', 'Extraordinaria']
+
 
 class ActaBase(BaseModel):
     id_acta: str | None = None
-    lenguage: Literal['Español', 'English', 'Català', 'Français', 'Chainese', 'Deutsch']
-    tipo: Literal['Ordinaria', 'Extraordinaria']
+    lenguaje: IDIOMAS_DISPONIBLES
+    tipo: TIPOS_ACTA
     fecha: datetime | None = None
     activa: bool
 
