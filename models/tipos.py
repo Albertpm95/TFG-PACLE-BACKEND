93f3801a9305cfd4202a863d503752aa647df365
Albert <albertpm95@outlook.com>
2023-02-20 23:11:46 +0100

    Renamed table

--- models/tipos.py
+++ models/tipos.py
@@ -3,7 +3,7 @@ from db.database import Base
 
 
 class Tipos(Base):
-    __tablename__ = "Tipos"
+    __tablename__ = "tipos"
 
     id_tipo = Column(Integer, primary_key=True)
     tipo = Column(String, nullable=False)
