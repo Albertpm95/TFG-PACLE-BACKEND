78edb6b06c7112667fe04e503de4a473d4931548
Albert <albertpm95@outlook.com>
2023-02-25 17:34:21 +0100

    Añadido el login con oauth2 y jwt al login router

--- routers/login.py
+++ routers/login.py
@@ -1,46 +1,40 @@
+from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
+
 from sqlalchemy.orm import Session
 
+from environment import ACCESS_TOKEN_EXPIRE_MINUTES
 
 from crud import crud
-from crud import usuario as crud_usuario
-from schemas.usuario import UsuarioLogin
+from crud import login as crud_login
+from schemas.token import Token
+
+from schemas.usuario import UsuarioBase
 
 router = APIRouter()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-def fake_decode_token(token, db: Session = Depends(crud.get_db)):
-    user = crud_usuario.get_user_username(db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Credenciales de autentificacion invalidas.",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-
-@router.post("/login")
-async def login(
+@router.post("/login", response_model=Token)
+async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(crud.get_db)
 ):
-    user_dict = crud_usuario.get_user_username(db, form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Usuario o contraseña incorrectos.")
-    usuario_sch = UsuarioLogin(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == usuario_sch.hashed_password:
-        raise HTTPException(status_code=400, detail="Usuario o contraseña incorrectos.")
+    usuario = crud_login.authenticate_user(form_data.username, form_data.password, db)
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuario o contraseña incorrectos.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = crud_login.create_access_token(
+        data={"sub": usuario.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-    return {"access_token": usuario_sch.username, "token_type": "bearer"}
+
+@router.get("/usuario/actual/", response_model=UsuarioBase)
+async def recuperar_usuario_actual(
+    current_user: UsuarioBase = Depends(crud_login.get_current_active_user),
+):
+    return current_user
