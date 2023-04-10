44fc236aafc85113e4670b06a5ecc9be8dce466e
Albert <albertpm95@outlook.com>
2023-04-10 22:15:18 +0200

    Added postgres metadata to alembic conf

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from db.database import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -44,7 +46,8 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-    )
+        version_table_schema="pacle_db"
+        )
 
     with context.begin_transaction():
         context.run_migrations()
@@ -61,6 +64,7 @@ def run_migrations_online() -> None:
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        version_table_schema="pacle_db"
     )
 
     with connectable.connect() as connection:
