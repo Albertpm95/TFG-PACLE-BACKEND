1f4fa709c49f71a83893f5cd8bdbb9a7d79c36cc
albertpm95 <albertpm95@outlook.com>
2023-04-13 23:04:50 +0200

    Updated import

--- crud/login.py
+++ crud/login.py
@@ -11,7 +11,7 @@ from models.usuario import Usuario
 from schemas.token import TokenData
 from schemas.usuario import UsuarioBase, UsuarioDB, UsuarioLogin
 
-
+from jose import JWTError, jwt
 from environment import ALGORITHM, SECRET_KEY
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -27,13 +27,13 @@ def authenticate_user(username: str, password: str, db: Session) -> UsuarioLogin
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
-    to_encode = data.copy()
+    to_encode: dict = data.copy()
     if expires_delta:
         expire: datetime = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt: str = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
@@ -52,7 +52,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UsuarioDB:
     except JWTError:
         raise credentials_exception
     db: Session = Depends(crud.get_db)
-    usuario = crud_usuario.get_user_username(db, token_data.username)
+    usuario: Usuario = crud_usuario.get_user_username(db, token_data.username)
     if usuario is None:
         raise credentials_exception
     return usuario
